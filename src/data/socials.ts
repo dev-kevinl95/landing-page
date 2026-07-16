27ea201e4c99e4f9c0148234043ea60d9ed183ce
Kevin Larico <kevin.larico955gmail.com>
2026-07-15 22:04:40 -0500

    data para socials

---
+++ src/data/socials.ts
@@ -0,0 +1,4 @@
+export const GITHUB_URL = 'https://github.com/tu-usuario';
+export const LINKEDIN_URL = 'https://linkedin.com/in/tu-usuario';
+export const EMAIL = 'tu@correo.com';
+export const CV_URL = '#';
